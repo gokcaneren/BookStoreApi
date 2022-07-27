a215a3824778d30c5ff487f6e9016340547de8f5
gokcaneren <gokcann01@gmail.com>
2022-07-27 13:51:34 +0300

    core layer completed!

---
+++ BookStore.Core/Services/IService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+
+    }
+}
---
+++ BookStore.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsnyc();
+        void Commit();
+    }
+}
