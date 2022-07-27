8f2f05bdaac2184359d0dbdacfa243a7b61d504f
gokcaneren <gokcann01@gmail.com>
2022-07-27 14:01:22 +0300

    BookConfiguration completed!

---
+++ BookStore.Repository/Configurations/BookConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository.Configurations
+{
+    internal class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            builder.Property(x=>x.AuthorName).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Stock).IsRequired();
+
+            builder.HasOne(c => c.Category).WithMany(b => b.Books).HasForeignKey(c => c.CategoryId);
+        }
+    }
+}
