dae6a9ea8531e3fdb87af37992eb00cc5a9bea16
gokcaneren <gokcann01@gmail.com>
2022-07-27 14:12:39 +0300

    Seeds completed!

---
+++ BookStore.Repository/Seeds/BookSeed.cs
@@ -0,0 +1,44 @@
+﻿using BookStore.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository.Seeds
+{
+    internal class BookSeed : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                new Book
+                {
+                    Id = 1,
+                    Name = "Haliç'te Yaşayan Simonlar",
+                    AuthorName = "Hanefi Avcı",
+                    CategoryId = 1,
+                    Stock = 25
+                },
+                new Book
+                {
+                    Id = 2,
+                    Name = "Silmarillion",
+                    AuthorName = "J.R.R Tolkien",
+                    CategoryId = 3,
+                    Stock = 35
+                },
+                new Book
+                {
+                    Id = 3,
+                    Name = "Hayvan Çiftliği",
+                    AuthorName = "Stephen King",
+                    CategoryId = 5,
+                    Stock = 12
+                }
+                );
+        }
+    }
+}
---
+++ BookStore.Repository/Seeds/CategorySeed.cs
@@ -0,0 +1,55 @@
+﻿using BookStore.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository.Seeds
+{
+    internal class CategorySeed : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Tarih-Araştırma",
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Bilim Kurgu",
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Fantastik",
+                },
+                new Category
+                {
+                    Id = 4,
+                    Name = "Polisiye",
+                },
+                new Category
+                {
+                    Id = 5,
+                    Name = "Korku-Gerilim",
+                },
+                new Category
+                {
+                    Id = 6,
+                    Name = "Gizem",
+                },
+                new Category
+                {
+                    Id = 7,
+                    Name = "Eğitim",
+                }
+                );
+        }
+    }
+}
