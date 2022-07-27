e52e28da86a8b955f5e32f843f97f9bdb5f2393a
gokcaneren <gokcann01@gmail.com>
2022-07-27 13:43:12 +0300

    Models are created!

---
+++ BookStore.Core/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.Models
+{
+    public class Book:BaseEntity
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public string AuthorName { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
