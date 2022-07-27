09ec85b3c97a07357405e7810fecd53a768a819e
gokcaneren <gokcann01@gmail.com>
2022-07-27 14:18:37 +0300

    User Entity, Configuration and Seed added!

---
+++ BookStore.Repository/Configurations/UserConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository.Configurations
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.UserName).IsRequired().HasMaxLength(15);
+            builder.Property(x => x.Password).IsRequired().HasMaxLength(15);
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(30);
+            builder.Property(x => x.Role).IsRequired().HasMaxLength(15);
+        }
+    }
+}
---
+++ BookStore.Repository/Seeds/UserSeed.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository.Seeds
+{
+    internal class UserSeed : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasData(
+                new User
+                {
+                    Id = 1,
+                    UserName = "goky",
+                    Password = "1995",
+                    Email = "gokk@yahoo.com",
+                    Role = "admin"
+                },
+                new User
+                {
+                    Id = 2,
+                    UserName = "burcu",
+                    Password = "1996",
+                    Email = "burcuu@yahoo.com",
+                    Role = "editor"
+                }
+                );
+        }
+    }
+}
