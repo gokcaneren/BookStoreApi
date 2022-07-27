63eb7d0c7f56b040d6eb711c807500d7cbfb41de
gokcaneren <gokcann01@gmail.com>
2022-07-27 13:56:29 +0300

    Starting to build repository layer

---
+++ BookStore.Repository/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+    }
+}
