f6684387d27ffe0d784600305d7cfce8dbbad714
gokcaneren <gokcann01@gmail.com>
2022-07-28 10:32:39 +0300

    service completed!

---
+++ BookStore.Service/Services/Service.cs
@@ -0,0 +1,53 @@
+﻿using BookStore.Core.Repositories;
+using BookStore.Core.Services;
+using BookStore.Core.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Service.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        private readonly IGenericRepository<T> _repository;
+        private readonly IUnitOfWork _unitOfWork;
+        public Service(IGenericRepository<T> repository, IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _repository.AddAsync(entity);
+            await _unitOfWork.CommitAsnyc();
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _repository.GetAll().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var book = await _repository.GetByIdAsync(id);
+            return book;
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            _repository.Remove(entity);
+            await _unitOfWork.CommitAsnyc();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            ;_repository.Update(entity);
+            await _unitOfWork.CommitAsnyc();    
+        }
+    }
+}
