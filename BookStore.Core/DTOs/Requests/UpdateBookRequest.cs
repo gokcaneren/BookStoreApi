2694285cf9986190e40052b3d9943b341ce27061
gokcaneren <gokcann01@gmail.com>
2022-07-28 09:59:14 +0300

    Dtos added

---
+++ BookStore.Core/DTOs/Requests/UpdateBookRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.DTOs.Requests
+{
+    public class UpdateBookRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public string AuthorName { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ BookStore.Core/DTOs/Respons/BookResponsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.DTOs.Respons
+{
+    public class BookResponsDto:BaseDto
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public string AuthorName { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
