3120205f8cc7315f006753c95a6ff36ac271016f
gokcaneren <gokcann01@gmail.com>
2022-07-28 10:04:24 +0300

    Automapper added to project

---
+++ BookStore.Service/Mapping/MapProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BookStore.Core.DTOs.Requests;
+using BookStore.Core.DTOs.Respons;
+using BookStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Service.Mapping
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Book, BookResponsDto>();
+            CreateMap<Category, CategoryResponsDto>();
+            CreateMap<AddBookRequest, Book>();
+            CreateMap<UpdateBookRequest, Book>();
+        }
+    }
+}
