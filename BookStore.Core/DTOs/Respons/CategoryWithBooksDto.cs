605bfe9310819d4ffe507f03de9f2442ddff739b
gokcaneren <gokcann01@gmail.com>
2022-08-02 11:15:22 +0300

    core layer completed for category controller

---
+++ BookStore.Core/DTOs/Respons/CategoryWithBooksDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.DTOs.Respons
+{
+    public class CategoryWithBooksDto:CategoryResponsDto
+    {
+        public List<BookResponsDto> Books { get; set; }
+    }
+}
