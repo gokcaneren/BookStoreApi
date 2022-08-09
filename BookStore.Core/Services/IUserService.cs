9dbc268b7ff71501925740ba86fb85803755580b
gokcaneren <gokcann01@gmail.com>
2022-08-09 12:22:12 +0300

    UserServices are completed!

---
+++ BookStore.Core/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.Services
+{
+    public interface IUserService:IService<User>
+    {
+        User ValidateUser(string username, string password);
+    }
+}
---
+++ BookStore.Service/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using BookStore.Core.Models;
+using BookStore.Core.Repositories;
+using BookStore.Core.Services;
+using BookStore.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Service.Services
+{
+    public class UserService : Service<User>, IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IGenericRepository<User> repository, IUnitOfWork unitOfWork, IUserRepository userRepository) : base(repository, unitOfWork)
+        {
+            _userRepository = userRepository;
+        }
+
+        public User ValidateUser(string username, string password)
+        {
+            var user= _userRepository.ValidateUser(username, password);
+            return user;
+        }
+    }
+}
