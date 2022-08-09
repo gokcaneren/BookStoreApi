ddb594700f94ae4b0afd7978e6ccb65a8841eb96
gokcaneren <gokcann01@gmail.com>
2022-08-09 12:18:44 +0300

    User repositories completed!

---
+++ BookStore.Repository/Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Core.Models;
+using BookStore.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository.Repositories
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public User ValidateUser(string username, string password)
+        {
+            return _context.Users.FirstOrDefault(x => x.UserName == username && x.Password == password);
+        }
+    }
+}
